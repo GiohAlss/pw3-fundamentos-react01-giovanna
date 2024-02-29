f57f8a1faac5c6034acfd3b2f676553297d367a0
GiohAlss <giovanna.almeida074@gmail.com>
2024-02-29 20:25:40 -0300

    Giovanna: Rederização dinâmica

--- fundamentos-react-01-giovanna/src/App.js
+++ fundamentos-react-01-giovanna/src/App.js
@@ -1,36 +1,13 @@
-import logo from './logo.svg';
+import dadosJSON  from './dados.json';
 import Livro from './components/Livro';
 import './App.css';
-
-import cavernas from './livros/cavernas_aco.jpg';
-import sol from './livros/sol_desvelado.jpg';
-import alvorada from './livros/robos_alvorada.jpg';
-
-const desc = "As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica."
 
 function App() {
   return (
     <div className="App">
-      <Livro
-        titulo="As Cavernas de Aço"
-        autor="Isaac Azimov"
-        imagem={cavernas}
-        descricao={desc}
-        />
 
-      <Livro
-        titulo="Sol Desvelado"
-        autor="Isaac Azimov"
-        imagem={sol}
-        descricao={desc}
-        />
-     
-     <Livro
-        titulo="Robôs de Alvorada"
-        autor="Isaac Azimov"
-        imagem={alvorada}
-        descricao={desc}
-        />
+      <Livro dadosJSON={dadosJSON}/>
+
     </div>
   );
 }
